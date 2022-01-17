9a3dfdaa5c271ba085eeb067ec43f85705875368
x1 <adnan.ligata@hotmail.com>
2022-01-17 19:28:45 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var a = prompt('Unesi poluprecnik osnove valjka "r"');
+var b = prompt('Unesi visinu valjka "H"');
+var c = 3.14;
+var rezultat = parseInt(a)*parseInt(a)*parseInt(b)*(c);
+document.write(`Zapremina valjka "V" je: ${rezultat}`);
